c4cfb9455d2c446eb0e8003072a5a19eac13ccdd
Tomokazu <atlof_fed_1win6_tomo@yahoo.co.jp>
2021-09-02 17:07:50 +0900

    change ssh key

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ server "192.168.33.10", user: "root", roles: %w{web db app}
 set :ssh_options, {
   user: "root",
   port: 22,
-  keys: %w(~/.ssh/id_ed25519),
+  keys: %w(~/.ssh/id_rsa_sub),
   forward_agent: true,
   auth_methods: %w[publickey]
 }
